e68c11bad350e0f575ad165b00248add54b10bad
TheCedarPrince <jacobszelko@gmail.com>
2021-12-01 15:15:57 -0500

    Attempted fix at deployment path issue

--- utils.jl
+++ utils.jl
@@ -87,18 +87,19 @@ end
 
 function hfun_insert_pandoc(params)
     rpath = params[1]
-    fullpath = rpath
-    print("Inserting References for $(params[1])... ")
+    file_path = joinpath(Franklin.path(:folder), rpath)
+    (isfile(file_path) && splitext(file_path)[2] == ".jmd") || return ""
     t = tempname()
     s = ""
     if length(params) == 2
+        cite_path = file_path = joinpath(Franklin.path(:folder), params[2])
         s = read(
-            `pandoc --citeproc -i $rpath --bibliography=$(params[2]) -t html`,
+            `pandoc --citeproc -i $file_path --bibliography=$cite_path -t html`,
             String,
         )
     else
         s = read(
-            `pandoc --citeproc -i $rpath -t html`,
+            `pandoc --citeproc -i $file_path -t html`,
             String,
         )
     end
